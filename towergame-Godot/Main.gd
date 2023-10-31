e288decc4aade81e270865c913a4d40f0aef4dce
Grant Goates <ggoates17@gmail.com>
2023-10-31 15:56:07 -0700

    added assets

--- towergame-Godot/Main.gd
+++ towergame-Godot/Main.gd
@@ -3,10 +3,7 @@ extends Node2D
 
 func _on_quit_pressed():
 	get_tree().quit()
-	
-
-
-
 
 func _on_play_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://world.tscn")
+

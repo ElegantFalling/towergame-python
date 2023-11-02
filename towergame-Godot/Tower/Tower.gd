f009c8e4b199f69705ce196fabb4df805566eda2
Zephram Tripp <elegantfalling@gmail.com>
2023-11-01 22:30:28 -0500

    More breaking

---
+++ towergame-Godot/Tower/Tower.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Tower Tower: ", get_rect())
+		print("Click Tower: ",to_local(event.position))
+		if get_rect().has_point((to_local(event.position))):
+			print("You clicked the tower!")
+		
